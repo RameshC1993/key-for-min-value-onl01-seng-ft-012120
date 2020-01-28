55d020ac12fd43c78dff5d1d058011a7e38343e4
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-28 20:08:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,5 @@ def key_for_min_value(hash)
     return nil
   end
   smallest_value = 0 
-  hash.each do |key, value| 
-    if value <= smallest_value
-      smallest_value += value
-    end
-  end
-  return smallest_value
+  hash.each do |key, value|
 end
