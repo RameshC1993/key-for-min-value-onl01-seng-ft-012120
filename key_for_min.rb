3718a85561ed18ffc1f3876191c913ab22139449
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-28 20:26:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@ def key_for_min_value(hash)
   if hash == {}
     return nil
   end
-  smallest_value =  
+  smallest_value = Infinity 
   small_value_key = nil 
   hash.each do |key, value|
     if 
