f78051633e4f0a180e29c640dc495eca204d516d
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-28 20:29:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,13 @@ def key_for_min_value(hash)
   if hash == {}
     return nil
   end
-  smallest_value = Infinity 
+  smallest_value = Float::INFINITY 
   small_value_key = nil 
   hash.each do |key, value|
-    if 
+    if value < smallest_value
+      smallest_value = value
+      small_value_key = key
+    end
   end
+  return small_value_key
 end
